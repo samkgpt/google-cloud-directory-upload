a63cc4ed5ae717d923c823f4e0c2f26da8ddd646
Shyam Gupta <shyamgupta047@gmail.com>
2021-03-11 19:54:10 +0530

    Add typescript declaration

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 /**
-To upload files and folder from a directory to google cloud bucket.
+To upload files and folder from a directory to google cloud storage bucket.
 @param bucketName - Google Cloud bucket name.
 @param keyFilename - Bucket configuration key file path.
 @param directoryPath - Directory to upload.
@@ -8,6 +8,7 @@ To upload files and folder from a directory to google cloud bucket.
 @returns The username for the `email` or `undefined` if it cannot be found.
 @example
 ```
+// usage
 import uploadDirectory  = require("google-cloud-directory-upload");
 const options = {
   bucketName: "bucketName",
@@ -23,6 +24,22 @@ const options = {
 (async () => {
   console.log(await uploadDirectory(options)); // returns <response_data>
 })();
+
+// response
+{
+  status: 200,
+  files: '7 files uploaded successfully',
+  fileList: [
+    '/tests/reports/mochareports/assets/app.css',
+    '/tests/reports/mochareports/assets/app.css.map',
+    '/tests/reports/mochareports/assets/app.js',
+    '/tests/reports/mochareports/assets/app.js.LICENSE.txt',
+    '/tests/reports/mochareports/assets/app.js.map',
+    '/tests/reports/mochareports/assets/MaterialIcons-Regular.woff',
+    '/tests/reports/mochareports/assets/MaterialIcons-Regular.woff2',
+  ],
+  bucketUrl: 'https://storage.googleapis.com/{bucketName}/{bucketPath}'
+}
 ```
 */
 
